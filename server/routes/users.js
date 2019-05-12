c6bb5b4bd04341b6216bb7f469d0a972b2f88add
filippopiconese <filippopiconese@hotmail.it>
2019-05-12 23:41:19 +0200

    feat(routes): added users route for google oauth

--- server/routes/users.js
+++ server/routes/users.js
@@ -14,6 +14,9 @@ router.route('/signup')
 router.route('/signin')
   .post(validateBody(schemas.authSchema), passportSignIn, UserController.signIn)
 
+router.route('/oauth/google')
+  .post(passport.authenticate('googleToken', { session: false }))
+
 router.route('/secret')
   .get(passportJWT, UserController.secret)
 
