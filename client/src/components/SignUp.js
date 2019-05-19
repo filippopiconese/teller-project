45b423270b4da3f251a2217f230f81e47313869e
filippopiconese <filippopiconese@hotmail.it>
2019-05-19 19:30:02 +0200

    feat(redux): Added redux dependencies to handle the signUp, signIn and the other Form actions

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import { reduxForm, Field } from 'redux-form'
 
-export default class SignUp extends Component {
+class SignUp extends Component {
   render() {
     return (
       <div>
-        This is a SignUp component
+        <form>
+          <fieldset>
+            <Field
+              name="email"
+              type="text"
+              id="email"
+              component="input"
+            />
+          </fieldset>
+          <fieldset>
+            <Field
+              name="password"
+              type="password"
+              id="password"
+              component="input"
+            />
+          </fieldset>
+
+          <button type="submit">Sign Up</button>
+        </form>
       </div>
     )
   }
 }
+
+export default reduxForm({ form: 'signup' })(SignUp)
