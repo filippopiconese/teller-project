32b5c90875761c7dd8c6875c03ad4560cfaaac51
filippopiconese <filippopiconese@hotmail.it>
2019-05-20 15:50:43 +0200

    feat(types): Dispatch user just signed up (with jwtToken)

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,14 +1,15 @@
 import axios from 'axios'
 
+import { AUTH_SIGN_UP } from './types'
 /*
   ActionCreators -> create/return Actions ({ }) -> dispatched -> middlewares -> reducers
 */
 
 export const signUp = data => {
   /*
-    Step 1) Use the form data and make HTTP request to our Back-End and send it along (axios library needed)
-    Step 2) Take the BE's response (jwtToken is here now!)
-    Step 3) Dispatch user just signed u (with jwtToken)
+    Step 1) Use the form data and make HTTP request to our Back-End and send it along (axios library needed) [X]
+    Step 2) Take the BE's response (jwtToken is here now!) [X]
+    Step 3) Dispatch user just signed u (with jwtToken) [X]
     Step 4) Save the jwtToken into our localStorage
   */
 
@@ -16,6 +17,11 @@ export const signUp = data => {
     try {
       const res = await axios.post('http://localhost:5000/users/signup', data)
       console.log('res', res)
+
+      dispatch({
+        type: AUTH_SIGN_UP,
+        payload: res.data.token
+      })
     } catch (error) {
       console.error(error)
     }
