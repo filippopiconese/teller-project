4cab72b5ce59fed1d8f5c7085a95b1f9adc6f99c
filippopiconese <filippopiconese@hotmail.it>
2019-05-20 14:58:03 +0200

    style(margin): added margins to the react SignUp page

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,30 +1,46 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
 
+import CustomInput from './Custominput'
 class SignUp extends Component {
   render() {
     return (
-      <div>
-        <form>
-          <fieldset>
-            <Field
-              name="email"
-              type="text"
-              id="email"
-              component="input"
-            />
-          </fieldset>
-          <fieldset>
-            <Field
-              name="password"
-              type="password"
-              id="password"
-              component="input"
-            />
-          </fieldset>
+      <div className="row">
+        <div className="col">
+          <form>
+            <fieldset>
+              <Field
+                name="email"
+                type="text"
+                id="email"
+                label="Enter your email"
+                placeholder="example@example.com"
+                component={CustomInput}
+              />
+            </fieldset>
+            <fieldset>
+              <Field
+                name="password"
+                type="password"
+                id="password"
+                label="Enter your password"
+                placeholder="yoursuperpassword"
+                component={CustomInput}
+              />
+            </fieldset>
 
-          <button type="submit">Sign Up</button>
-        </form>
+            <button type="submit" className="btn btn-primary">Sign Up</button>
+          </form>
+        </div>
+        <div className="col">
+          <div className="text-center">
+            <div className="alert alert-primary">
+              Sin Up
+            </div>
+            <button className="btn btn-default">Facebook</button>
+            <button className="btn btn-default">Google</button>
+          </div>
+        </div>
       </div>
     )
   }
