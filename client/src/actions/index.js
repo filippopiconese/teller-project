0d5e66ad2f9094b49868175170e96c66e73df782
filippopiconese <filippopiconese@hotmail.it>
2019-05-20 15:51:56 +0200

    feat(jwt_token): Save the jwtToken into our localStorage

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -9,8 +9,8 @@ export const signUp = data => {
   /*
     Step 1) Use the form data and make HTTP request to our Back-End and send it along (axios library needed) [X]
     Step 2) Take the BE's response (jwtToken is here now!) [X]
-    Step 3) Dispatch user just signed u (with jwtToken) [X]
-    Step 4) Save the jwtToken into our localStorage
+    Step 3) Dispatch user just signed up (with jwtToken) [X]
+    Step 4) Save the jwtToken into our localStorage [X]
   */
 
   return async dispatch => {
@@ -22,6 +22,8 @@ export const signUp = data => {
         type: AUTH_SIGN_UP,
         payload: res.data.token
       })
+
+      localStorage.setItem('JWT_TOKEN', res.data.token)
     } catch (error) {
       console.error(error)
     }
